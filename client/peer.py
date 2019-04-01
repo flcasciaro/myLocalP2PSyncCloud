e144241d833b60ce372799e4631ce1bec39d8bfe
flcasciaro <fl.casciaro@gmail.com>
2019-04-01 23:39:00 +0200

    Added handshake with unique peerID and modified lightly some of the functions.

--- client/peer.py
+++ client/peer.py
@@ -5,7 +5,10 @@
 import hashlib
 import socket
 import time
+import uuid
+#import os
 
+configurationFile = "conf.txt"
 
 def createSocket(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,6 +27,22 @@ def closeSocket(sock):
     time.sleep(1)
     sock.close()
 
+def handshake(serverIP, serverPort, peerID):
+
+    s = createSocket(serverIP, serverPort)
+
+    message = "I'M {}".format(peerID)
+    s.send(message.encode('ascii'))
+
+    answer = s.recv(1024).decode('ascii').strip()
+
+    if answer != "HELLO {}".format(peerID):
+        print ("Unable to perform the initial handshake with the server")
+        return None
+
+    print("Connection with server established")
+    return s
+
 
 def retrieveGroupsList(s, previous):
     if previous:
@@ -40,92 +59,97 @@ def retrieveGroupsList(s, previous):
     return groupsList
 
 
-def restoreGroups(serverIP, serverPort):
-    s = createSocket(serverIP, serverPort)
+def restoreGroups(s):
 
-    print("Retrieving information about previous session..")
+    print("Retrieving information about previous sessions..")
     previousGroupsList = retrieveGroupsList(s, previous=True)
 
     print("List of synchronization groups that you have already joined:")
     for group in previousGroupsList:
-        print(group)
+        print("GroupName: {} \tActive members: {} \tTotal members: {}"
+              .format(group["name"], group["active"], group["total"]))
 
-    while True:
-        groupName = input("Write the name of the group you want to restore: ")
+    choice = input("Do you want to restore a synchronization session with one of these groups? (y/n): ")
 
-        message = "RESTORE Group: {}".format(groupName)
-        print(message)
-        s.send(message.encode('ascii'))
+    if choice.upper() == 'Y':
+        while True:
+            groupName = input("Write the name of the group you want to restore: ")
 
-        data = s.recv(1024)
-        print('Received from the server :', str(data.decode('ascii')))
+            message = "RESTORE Group: {}".format(groupName)
+            print(message)
+            s.send(message.encode('ascii'))
 
-        choice = input("Do you want to restore another group? (y/n)")
+            data = s.recv(1024)
+            print('Received from the server :', str(data.decode('ascii')))
 
-        if choice.upper() != "Y":
-            break
+            choice = input("Do you want to restore another group? (y/n): ")
 
-    closeSocket(s)
+            if choice.upper() != "Y":
+                break
 
 
-def joinGroups(serverIP, serverPort):
-    s = createSocket(serverIP, serverPort)
+
+def joinGroups(s):
 
     print("Retrieving other groups list..")
     newGroupsList = retrieveGroupsList(s, previous=False)
 
-    print("List of synchronization groups available:")
+    print("List of synchronization groups available (never joined):")
     for group in newGroupsList:
         print(group)
 
-    while True:
+    choice = input("Do you want to restore a synchronization session with one of these groups? (y/n): ")
 
-        groupName = input("Write the name of the group you want to join: ")
-        groupToken = input("Write the token of the group you want to access: ")
+    if choice.upper() == 'Y':
+        while True:
 
-        encryptedToken = hashlib.md5(groupToken.encode())
+            groupName = input("Write the name of the group you want to join: ")
+            groupToken = input("Write the token of the group you want to access: ")
 
-        message = "JOIN Group: {} Token: {}".format(groupName, encryptedToken.hexdigest())
-        # print(message)
-        s.send(message.encode('ascii'))
+            encryptedToken = hashlib.md5(groupToken.encode())
 
-        data = s.recv(1024)
-        print('Received from the server :', str(data.decode('ascii')))
+            message = "JOIN Group: {} Token: {}".format(groupName, encryptedToken.hexdigest())
+            # print(message)
+            s.send(message.encode('ascii'))
 
-        choice = input("Do you want to join another group? (y/n)")
+            data = s.recv(1024)
+            print('Received from the server :', str(data.decode('ascii')))
 
-        if choice.upper() != "Y":
-            break
+            choice = input("Do you want to join another group? (y/n): ")
 
-    closeSocket(s)
+            if choice.upper() != "Y":
+                break
 
 
-def createGroups(serverIP, serverPort):
-    s = createSocket(serverIP, serverPort)
 
-    while True:
+def createGroups(s):
 
-        groupName = input("Write the name of the group you want to create: ")
-        groupTokenRW = input("Write the token for Reader&Writer of the group you want to create: ")
-        groupTokenRO = input("Write the token for ReadOnly of the group you want to create: ")
+    choice = input("Do you want to create a new synchronization group? (y/n): ")
 
-        encryptedTokenRW = hashlib.md5(groupTokenRW.encode())
-        encryptedTokenRO = hashlib.md5(groupTokenRO.encode())
+    if choice.upper() == 'Y':
 
-        message = "CREATE Group: {} TokenRW: {} TokenRO: {}".format(groupName,
-                                                                    encryptedTokenRW.hexdigest(),
-                                                                    encryptedTokenRO.hexdigest())
-        s.send(message.encode('ascii'))
+        while True:
 
-        data = s.recv(1024)
-        print('Received from the server :', str(data.decode('ascii')))
+            groupName = input("Write the name of the group you want to create: ")
+            groupTokenRW = input("Write the token for Reader&Writer of the group you want to create: ")
+            groupTokenRO = input("Write the token for ReadOnly of the group you want to create: ")
 
-        choice = input("Do you want to create another group? (y/n)")
+            encryptedTokenRW = hashlib.md5(groupTokenRW.encode())
+            encryptedTokenRO = hashlib.md5(groupTokenRO.encode())
 
-        if choice.upper() != "Y":
-            break
+            message = "CREATE Group: {} TokenRW: {} TokenRO: {}".format(groupName,
+                                                                        encryptedTokenRW.hexdigest(),
+                                                                        encryptedTokenRO.hexdigest())
+            s.send(message.encode('ascii'))
 
-    closeSocket(s)
+            data = s.recv(1024)
+            print('Received from the server :', str(data.decode('ascii')))
+
+            choice = input("Do you want to create another group? (y/n): ")
+
+            if choice.upper() != "Y":
+                break
+
 
 
 if __name__ == '__main__':
@@ -135,7 +159,7 @@ if __name__ == '__main__':
 
     print("Retrieving configuration..")
     try:
-        file = open('conf.txt', 'r')
+        file = open(configurationFile, 'r')
         configuration = file.readline().split()
         serverIP = configuration[0]
         serverPort = int(configuration[1])
@@ -144,14 +168,28 @@ if __name__ == '__main__':
         serverIP = input("Insert server IP:")
         serverPort = input("Insert server port:")
         """"save configuration on the file"""
-        file = open('conf.txt', 'w')
+        file = open(configurationFile, 'w')
         file.write("{} {}".format(serverIP, serverPort))
     file.close()
 
+    """peer unique Identifier obtained from the MAC address of the machine"""
+    macAddress = uuid.getnode()
+    #peerID = int(time.ctime(os.path.getctime(configurationFile))) & macAddress
+    peerID = macAddress
+
+    ssock = handshake(serverIP, serverPort, peerID)
+
+    if ssock is None:
+        exit(-1)
+
     """"Let the user restores previous synchronization groups"""
-    #restoreGroups(serverIP, serverPort)
+    restoreGroups(ssock)
 
     """Let the user joins new synchronization groups"""
-    #joinGroups(serverIP, serverPort)
+    joinGroups(ssock)
 
-    createGroups(serverIP, serverPort)
+    createGroups(ssock)
+
+    print("Sync happens here...")
+
+    closeSocket(ssock)
