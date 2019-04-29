506e15dff91507903e9f7d3a52bb526b6eab461e
flcasciaro <fl.casciaro@gmail.com>
2019-04-29 18:21:19 +0200

    Server functionalities implemented

--- client/peerCore.py
+++ client/peerCore.py
@@ -3,10 +3,12 @@
 """@author: Francesco Lorenzo Casciaro - Politecnico di Torino - UPC"""
 
 import hashlib
+import queue
+import select
 import socket
 import time
 import uuid
-#import os
+from threading import Thread
 
 configurationFile = "conf.txt"
 peerID = None
@@ -17,6 +19,8 @@ serverPort = None
 activeGroupsList = {}
 restoreGroupsList = {}
 otherGroupsList = {}
+
+queue = queue.Queue()
 
 def setPeerID():
     global peerID
@@ -188,16 +192,17 @@ def changeRole(groupName, targetPeerID, action):
     else:
         return True
 
-def retrievePeers(groupName, all):
+def retrievePeers(groupName, selectAll):
 
     s = handshake()
 
-    if all:
+    if selectAll:
         tmp = "ALL"
     else:
         tmp = "ACTIVE"
 
     message = "PEERS {} {} ".format(groupName, tmp)
+    print(message)
     s.send(message.encode('ascii'))
 
     data = s.recv(1024)
@@ -261,18 +266,17 @@ def disconnectGroup(groupName):
 
 
 
-def startSync():
+def startSync(gui):
+
+    """retrieve internal IP address"""
+    myIP = socket.gethostbyname(socket.gethostname())
+    portNumber = 12321
 
     """create a server thread that listens on the port X"""
-    #createServer
+    server = Server(gui, myIP, portNumber)
+    server.start()
 
     s = handshake()
-
-    #retrieve internal IP address
-    myIP = socket.gethostbyname(socket.gethostname())
-    print(myIP)
-
-    portNumber = 10000
 
     message = "HERE {} {}".format(myIP, portNumber)
     s.send(message.encode('ascii'))
@@ -281,3 +285,119 @@ def startSync():
     print('Received from the server :', str(data.decode('ascii')))
 
     closeSocket(s)
+
+
+class Server(Thread):
+    def __init__(self, gui, host, port, max_clients = 10):
+        Thread.__init__(self)
+        """ Initialize the server with a host and port to listen to.
+        Provide a list of functions that will be used when receiving specific data """
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((host, port))
+        self.sock.listen(max_clients)
+        self.sock_threads = []
+        self.counter = 0 # Will be used to give a number to each thread, can be improved (re-assigning free number)
+        self.gui = gui
+        self.__stop = False
+
+    def run(self):
+        """ Accept an incoming connection.
+        Start a new SocketServerThread that will handle the communication. """
+        print('Starting socket server (host {}, port {})'.format(self.host,self.port))
+
+        while not self.__stop:
+            self.sock.settimeout(1)
+            try:
+                client_sock, client_addr = self.sock.accept()
+            except socket.timeout:
+                client_sock = None
+
+            if client_sock:
+                client_thr = SocketServerThread(client_sock, client_addr, self.counter, self.gui)
+                self.counter += 1
+                self.sock_threads.append(client_thr)
+                client_thr.start()
+
+        self.closeServer()
+
+    def closeServer(self):
+        """ Close the client socket threads and server socket if they exists. """
+        print('Closing server socket (host {}, port {})'.format(self.host, self.port))
+
+        for thr in self.sock_threads:
+            thr.stop()
+            thr.join()
+
+        if self.sock:
+            self.sock.close()
+
+    def stopServer(self):
+        """This function will be called in order to stop the server (example using the X on the GUI or a signal)"""
+        self.__stop = True
+
+
+class SocketServerThread(Thread):
+    def __init__(self, client_sock, client_addr, number, gui):
+        """ Initialize the Thread with a client socket and address """
+        Thread.__init__(self)
+        self.client_sock = client_sock
+        self.client_addr = client_addr
+        self.number = number
+        self.peerID = None  #it will be set after the connection establishment (socket creation)
+        self.gui = gui
+        self.__stop = False
+
+    def run(self):
+        print("[Thr {}] SocketServerThread starting with client {}".format(self.number, self.client_addr))
+
+        while not self.__stop:
+            if self.client_sock:
+                # Check if the client is still connected and if data is available:
+                try:
+                    rdy_read, rdy_write, sock_err = select.select([self.client_sock, ], [self.client_sock, ], [], 5)
+                except select.error:
+                    print('[Thr {}] Select() failed on socket with {}'.format(self.number,self.client_addr))
+                    self.stop()
+                    return
+
+                if len(rdy_read) > 0:
+                    read_data = self.client_sock.recv(1024)
+
+                    # Check if socket has been closed
+                    if len(read_data) == 0:
+                        print('[Thr {}] {} closed the socket.'.format(self.number, self.client_addr))
+                        self.stop()
+                    else:
+                        # Strip newlines just for output clarity
+                        message = read_data.decode('ascii').rstrip()
+                        manageRequest(self, message, self.gui)
+            else:
+                print("[Thr {}] No client is connected, SocketServer can't receive data".format(self.number))
+                self.stop()
+        self.close()
+
+    def stop(self):
+        self.__stop = True
+
+    def close(self):
+        """ Close connection with the client socket. """
+        if self.client_sock:
+            print('[Thr {}] Closing connection with {}'.format(self.number, self.client_addr))
+            self.client_sock.close()
+
+def manageRequest(self, message, gui):
+
+    """Serves the client request"""
+    print('[Thr {}] Received {}'.format(self.number, message))
+
+    if message == "EHI":
+        queue.put_nowait("ciao")
+        answer = "OK"
+        self.client_sock.send(answer.encode('ascii'))
+    elif message == "BYE":
+        answer = "BYE PEER"
+        self.client_sock.send(answer.encode('ascii'))
+        self.stop()
