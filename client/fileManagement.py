0a3bb30b432276a33c2a38d93628876a66215d58
flcasciaro <fl.casciaro@gmail.com>
2019-05-31 13:53:55 +0200

    Added empty files handling

--- client/fileManagement.py
+++ client/fileManagement.py
@@ -11,14 +11,15 @@ BIGGEST_SMALL_FILE_SIZE = 32 * 1048576      #   32 MB
 
 class File:
 
-    def __init__(self, groupName, filename, filepath, filesize, timestamp, previousChunks, status):
+    def __init__(self, groupName, filename, filepath, filesize, timestamp, status, previousChunks):
         self.groupName = groupName
         self.filename = filename
         self.filepath = filepath
         self.filesize = int(filesize)
         self.timestamp = int(timestamp)
-        self.previousChunks = previousChunks
         self.status = status
+        self.previousChunks = previousChunks
+
 
 
         """properties useful for the file-sharing"""
@@ -51,20 +52,31 @@ class File:
             print("File not found")
 
     def setChunksSize(self):
-        if self.filesize <= BIGGEST_SMALL_FILE_SIZE:
+
+        if self.filesize == 0:
+            # special case: empty file
+            self.chunksSize = 0
+        elif self.filesize <= BIGGEST_SMALL_FILE_SIZE:
             self.chunksSize = SMALL_CHUNK_SIZE
         else:
             self.chunksSize = BIG_CHUNK_SIZE
 
     def setProgress(self):
-        self.progress = math.floor((len(self.availableChunks) / self.chunksNumber) * 100 )
-
+        try:
+            self.progress = math.floor((len(self.availableChunks) / self.chunksNumber) * 100 )
+        except ZeroDivisionError:
+            #empty file
+            self.progress = 100
 
     def initDownload(self):
         """initialize all the properties in order to work as peer (download/upload)"""
         self.setChunksSize()
-        self.chunksNumber = math.ceil(self.filesize / self.chunksSize)
-        self.lastChunkSize = self.filesize % self.chunksSize
+        try:
+            self.chunksNumber = math.ceil(self.filesize / self.chunksSize)
+            self.lastChunkSize = self.filesize % self.chunksSize
+        except ZeroDivisionError:
+            self.chunksNumber = 0
+            self.lastChunkSize = 0
         self.missingChunks = list()
         self.availableChunks = list()
         for i in range(0, self.chunksNumber):
@@ -116,8 +128,8 @@ def getPreviousFiles(previousSessionFile):
                                  fileListJson[fileKey]["filepath"],
                                  fileListJson[fileKey]["filesize"],
                                  fileListJson[fileKey]["timestamp"],
-                                 fileListJson[fileKey]["previousChunks"],
-                                 status = "")
+                                 fileListJson[fileKey]["status"],
+                                 fileListJson[fileKey]["previousChunks"])
     del fileListJson
 
     return fileList
@@ -134,7 +146,9 @@ def saveFileStatus(previousSessionFile, fileList):
         fileListJson[fileKey]["filepath"] = fileList[fileKey].filepath
         fileListJson[fileKey]["filesize"] = fileList[fileKey].filesize
         fileListJson[fileKey]["timestamp"] = fileList[fileKey].timestamp
+        fileListJson[fileKey]["status"] = fileList[fileKey].status
         fileListJson[fileKey]["previousChunks"] = fileList[fileKey].previousChunks
+
 
     try:
         f = open(previousSessionFile, 'w')
