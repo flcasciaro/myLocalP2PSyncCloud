8f083264d588f71e45a34ddc479944f2b5edd729
flcasciaro <fl.casciaro@gmail.com>
2019-06-04 16:23:21 +0200

    Moved server into a specific file (from peerCore)

---
+++ client/peerServer.py
@@ -0,0 +1,122 @@
+import select
+import socket
+from threading import Thread
+
+import fileSharing
+import transmission
+
+
+class Server(Thread):
+    def __init__(self, host, port, max_clients=5):
+        Thread.__init__(self)
+        """ Initialize the server with a host and port to listen to.
+        Provide a list of functions that will be used when receiving specific data """
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((host, port))
+        self.sock.listen(max_clients)
+        self.sock_threads = []
+        self.counter = 0  # Will be used to give a number to each thread, can be improved (re-assigning free number)
+        self.__stop = False
+
+    def run(self):
+        """ Accept an incoming connection.
+        Start a new SocketServerThread that will handle the communication. """
+        print('Starting socket server (host {}, port {})'.format(self.host, self.port))
+
+        while not self.__stop:
+            self.sock.settimeout(1)
+            try:
+                client_sock, client_addr = self.sock.accept()
+            except socket.timeout:
+                client_sock = None
+
+            if client_sock:
+                client_thr = SocketServerThread(client_sock, client_addr, self.counter)
+                self.counter += 1
+                self.sock_threads.append(client_thr)
+                client_thr.daemon = True
+                client_thr.start()
+
+        self.closeServer()
+
+    def closeServer(self):
+        """ Close the client socket threads and server socket if they exists. """
+        print('Closing server socket (host {}, port {})'.format(self.host, self.port))
+
+        for thr in self.sock_threads:
+            thr.stop()
+            thr.join()
+
+        if self.sock:
+            self.sock.close()
+
+    def stopServer(self):
+        """This function will be called in order to stop the server (example using the X on the GUI or a signal)"""
+        self.__stop = True
+
+
+class SocketServerThread(Thread):
+    def __init__(self, client_sock, client_addr, number):
+        """ Initialize the Thread with a client socket and address """
+        Thread.__init__(self)
+        self.client_sock = client_sock
+        self.client_addr = client_addr
+        self.number = number
+        self.__stop = False
+
+    def run(self):
+        print("[Thr {}] SocketServerThread starting with peer {}".format(self.number, self.client_addr))
+
+        while not self.__stop:
+            if self.client_sock:
+                # Check if the client is still connected and if data is available:
+                try:
+                    rdy_read, rdy_write, sock_err = select.select([self.client_sock, ], [self.client_sock, ], [], 5)
+                except select.error:
+                    print('[Thr {}] Select() failed on socket with {}'.format(self.number, self.client_addr))
+                    self.stop()
+                    return
+
+                if len(rdy_read) > 0:
+                    read_data = transmission.myRecv(self.client_sock)
+
+                    # Check if socket has been closed
+                    if len(read_data) == 0:
+                        # print('[Thr {}] {} closed the socket.'.format(self.number, self.client_addr))
+                        self.stop()
+                    else:
+                        # Strip newlines just for output clarity
+                        message = read_data.rstrip()
+                        manageRequest(self, message)
+            else:
+                print("[Thr {}] No peer is connected, SocketServer can't receive data".format(self.number))
+                self.stop()
+        self.close()
+
+    def stop(self):
+        self.__stop = True
+
+    def close(self):
+        """ Close connection with the client socket. """
+        if self.client_sock:
+            print('[Thr {}] Closing connection with {}'.format(self.number, self.client_addr))
+            self.client_sock.close()
+
+
+def manageRequest(self, message):
+    """Serves the client request"""
+    print('[Thr {}] Received {}'.format(self.number, message))
+
+    if message.split()[0] == "CHUNKS_LIST":
+        fileSharing.sendChunksList(message, self)
+
+    if message.split()[0] == "CHUNK":
+        fileSharing.sendChunk(message, self)
+
+    elif message == "BYE":
+        answer = "BYE PEER"
+        transmission.mySend(self.client_sock, answer)
+        self.stop()
