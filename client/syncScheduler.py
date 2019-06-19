0afead4f3ab6028abffbb8c70380c41e855dafdd
flcasciaro <fl.casciaro@gmail.com>
2019-06-19 16:27:46 +0200

    Fixed bug

--- client/syncScheduler.py
+++ client/syncScheduler.py
@@ -228,7 +228,7 @@ def updatedFiles(message):
 
                     queueLock.acquire()
                     # remove outdated syncTask
-                    queue = [task for task in queue if not task.outdatedTask(newTask)]
+                    queue = deque([task for task in queue if not task.outdatedTask(newTask)])
                     # add newTask
                     queue.append(newTask)
                     queueLock.release()
