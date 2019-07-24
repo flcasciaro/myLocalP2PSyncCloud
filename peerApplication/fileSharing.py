7077ce649afc5763af143fbd7d715603993ed9b7
flcasciaro <fl.casciaro@gmail.com>
2019-07-24 21:22:38 +0200

    Changed random discard parameters

--- peerApplication/fileSharing.py
+++ peerApplication/fileSharing.py
@@ -44,7 +44,7 @@ CHECK_PERIOD = 1.0
 # parameters for chunks random discard
 INITIAL_TRESHOLD = 0.5
 TRESHOLD_INC_STEP = 0
-COMPLETION_RATE = 0.95
+COMPLETION_RATE = 0.99
 
 # maximum number of getChunk request leading to an error allowed before to quit a connection
 MAX_ERRORS = 3
